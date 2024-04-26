db5d81b7824de699cd0f869dea1f63392c8b40d8
Your Name <511172176@m365.fju.edu.tw>
2024-04-26 18:30:58 +0800

    update

---
+++ uva100.java
@@ -0,0 +1,39 @@
+import java.util.*;
+ 
+public class uva100 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNextLine()) {
+            
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int len = 0;
+            System.out.print(a+" "+b+" ");
+
+            for(int i=Math.min(a, b); i<=Math.max(a, b); i++)
+            {
+
+                len = Math.max(len(i), len);
+                
+            }
+            System.out.println(len);
+        }
+    }
+
+    public static int len(int n)
+    {
+        
+        int leng = 1;
+        while(n!=1)
+        {
+
+            if(n%2==1)
+                n = 3*n+1;
+            else if(n%2==0)
+                n /=2;
+            leng++;
+        }
+        return leng;
+    }
+}
--- uva10107.java
+++ uva10107.java
@@ -1,4 +1,4 @@
-package uva;
+//package uva;
 
 import java.util.ArrayList;
 import java.util.Arrays;
