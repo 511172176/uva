3d50ef24bd87cbabeefd929382233a1267630c1e
511172176 <511172176@m365.fju.edu.tw>
2024-09-02 00:14:03 +0800

    up

--- uva10062.cpp
+++ uva10062.cpp
@@ -10,16 +10,16 @@ struct charpair{
 };
 
 bool compare(const charpair &a, const charpair &b){
-    if(a.cnt!=b.cnt)
-        return a.cnt<b.cnt;
+    if(a.cnt != b.cnt)
+        return a.cnt < b.cnt;
     else
-        return a.charasc>b.charasc;
+        return a.charasc > b.charasc;
 }
 
 void print(const vector<charpair> &data){
     int i;
-    for(i=0; i<95; i++){
-        if(data[i].cnt!=0)
+    for(i = 0; i < 95; i++){
+        if(data[i].cnt != 0)
             printf("%d %d\n", data[i].charasc, data[i].cnt);
     }
 }
@@ -27,25 +27,35 @@ void print(const vector<charpair> &data){
 int main(){
 
     vector<charpair> data(95);
-    char c[9999];
-    int i, len, flag=0;
-    while(fgets(c, sizeof(c), stdin) != 0){
-        c[strcspn(c, "\n")] = '\0';
-        for(i=0; i<95; i++){
-            data[i].charasc = i+32;
-            data[i].cnt = 0;
-        }
+    char gc;
+    string c;
+    int i, flag = 0;
 
-        if(flag)
-            printf("\n");
-        flag = 1;
+    while(scanf("%c", &gc) == 1){
 
-        for(i=0; i<strlen(c); i++){
-            if(c[i]>=32 && c[i]<=126)
-                data[c[i]-32].cnt++;
+        if(gc == '\n'){
+
+            if(flag)
+                printf("\n");
+            flag = 1;
+
+            for(i = 0; i < 95; i++){
+                data[i].charasc = i + 32;
+                data[i].cnt = 0;
+            }
+
+            for(i = 0; i < c.length(); i++){
+                    data[c[i] - 32].cnt++;
+            }
+
+            sort(data.begin(), data.end(), compare);
+            print(data);
+
+            c.clear();
+        } else {
+            c.push_back(gc);
         }
-        sort(data.begin(), data.end(), compare);
-        print(data);
     }
+
     return 0;
 }
